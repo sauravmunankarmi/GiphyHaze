06c169e10b2e7c03c2a51fb7e5f93a25306d5db2
Saurav Munankarmi <sauravmunankarmi@gmail.com>
2022-03-15 15:33:44 +0545

    Splash Activity added

--- app/src/main/java/com/hazesoft/giphyhaze/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/hazesoft/giphyhaze/ui/mainActivity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hazesoft.giphyhaze
+package com.hazesoft.giphyhaze.ui.mainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hazesoft.giphyhaze.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/hazesoft/giphyhaze/ui/splashActivity/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.hazesoft.giphyhaze.ui.splashActivity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.hazesoft.giphyhaze.R
+import com.hazesoft.giphyhaze.ui.mainActivity.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
